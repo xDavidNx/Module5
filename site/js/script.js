41b28527f6c43b052090eb74aa91f4e4b2d4920d
David Niño <davidninosuarez0603@gmail.com>
2021-10-01 15:43:02 -0500

    My module5.

--- site/js/script.js
+++ site/js/script.js
@@ -13,7 +13,7 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
 var dc = {};
 
-var homeHtmlUrl = "../module5/snippets/home-snippet.html";
+var homeHtmlUrl = "../site/snippets/home-snippet.html";
 var allCategoriesUrl = 
   "https://davids-restaurant.herokuapp.com/categories.json";
 var categoriesTitleHtml = "snippets/categories-title-snippet.html";
